78f1f552a828d31bd40d749d5bacdb90ce849b62
Chirag Singhal <csinghal@indeed.com>
2023-05-04 02:18:23 +0530

    add boto3 code

--- generate_questions.py
+++ generate_questions.py
@@ -3,20 +3,32 @@ from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
 import spacy
 
+import boto3
+import os
 
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
 
-# Specify the name of the S3 bucket and the checkpoint file name
+# Specify the S3 bucket and directory that contains the checkpoint files
 bucket_name = 'glyphic-ai'
-checkpoint_file_name = 'checkpoint-11000'
+directory_name = 'checkpoint-11000'
 
-# Load the checkpoint file from the S3 bucket
-obj = s3.get_object(Bucket=bucket_name, Key=checkpoint_file_name)
-checkpoint_data = obj['Body'].read()
+# Specify the local directory on Elastic Beanstalk where you want to save the checkpoint files
+local_directory = '/checkpoint-11000'
+
+# Create the local directory if it doesn't exist
+if not os.path.exists(local_directory):
+    os.makedirs(local_directory)
+
+# Download all files in the S3 directory to the local directory
+bucket = s3.Bucket(bucket_name)
+for obj in bucket.objects.filter(Prefix=directory_name):
+    if not os.path.exists(os.path.dirname(local_directory + obj.key)):
+        os.makedirs(os.path.dirname(local_directory + obj.key))
+    bucket.download_file(obj.key, local_directory + obj.key.split("/")[-1])
 
 
-ckpt_path = './checkpoint/checkpoint-11000'
-model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint_data, force_download=False)
+ckpt_path = '/checkpoint-11000'
+model = AutoModelForSeq2SeqLM.from_pretrained(ckpt_path, force_download=False)
 
 model_checkpoint = "t5-base"
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
